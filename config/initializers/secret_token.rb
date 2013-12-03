46ff2dc329906096d1e9eaf5ad408ab9aa377c10
justvitalius <justvitalius@gmail.com>
2013-12-03 13:17:28 +0400

    пустое приложение

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MagicAuction::Application.config.secret_key_base = '1ec097b5af23e8f01849bd6d36d4b7463930cfb5886487f5352fd6c6f38863e6424a73fa6975a4ade48dab2bb0b66137f964b7a610f2da027a75d2d2ae071bab'
