82b3eb1274894081b032a3099dca319db534b6c2
justvitalius <justvitalius@gmail.com>
2013-12-11 12:21:01 +0400

    Add model and tests

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Category do
+  it 'should validates presence of title'
+  it 'should have title length less then 64 chars'
+
+  it 'should have #image'
+
+  it 'should have #children'
+  it 'should have #parent'
+end
