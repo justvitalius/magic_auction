44ddc8c5a7c271207d8aae080dade61adfe17959
justvitalius <justvitalius@gmail.com>
2013-12-11 12:15:23 +0400

    expand the list of tests.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe Product do
   it 'should validates presence of title'
+  it 'should have title length less then 64 chars'
+  it 'should have image'
+  it 'should validates presence of price'
+  it 'should validates price format'
+  it 'should have price length less then 10 chars'
+  it 'should have description'
+  it 'should have description length less then 1000 chars'
 end
