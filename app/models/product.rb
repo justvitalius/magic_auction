63990dd1468d7ffa4b0b73e6b18d5e2e97425b62
justvitalius <justvitalius@gmail.com>
2013-12-11 20:22:13 +0400

    Add tests and validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   validates :title, presence: true, length: {maximum: 64}
   validates :description, allow_blank: true, length: {maximum: 1000}
-  validates_numericality_of :price, presence: true, less_than: 1000000, only_integer: true
+  validates_numericality_of :price, presence: true, only_integer: true
+  validates_length_of :price, in: 1..10
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,16 +2,21 @@ require 'spec_helper'
 
 describe Product do
 
+  it {should respond_to(:title)}
   it {should validate_presence_of(:title)}
   it {should ensure_length_of(:title).is_at_most(64)}
 
 
   it 'should have image'
 
+  it {should respond_to(:price)}
   it {should validate_presence_of(:price)}
-  it {should validate_numericality_of(:price).is_less_than(1000000).only_integer}
+  it {should validate_numericality_of(:price).only_integer}
+  it {should validate_numericality_of(:price).is_less_than_or_equal_to(1000000)}
+  it {should validate_numericality_of(:price).is_greater_than_or_equal_to(1)}
   it 'should validate price integer parts and cents'
 
+  it {should respond_to(:description)}
   it {should ensure_length_of(:description).is_at_most(1000)}
 
   it 'should have many Auctions'
