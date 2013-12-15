d0ba332d1c4c54200be12d26148f9b2d458233e3
justvitalius <justvitalius@gmail.com>
2013-12-15 04:51:39 +0400

    New category form works fine.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,10 +2,13 @@ class CategoriesController < ResourcesController
   respond_to :html
 
 
+  def create
+    create!{ categories_path }
+  end
 
   protected
   def resource_params
-    [params.require(:category).permit(:title, :ancestry, :image)] if params.has_key?(:category)
+    [params.require(:category).permit(:title, :parent_id, :image)] if params.has_key?(:category)
   end
 
 end
