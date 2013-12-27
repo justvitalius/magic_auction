d71bef25317cb1575572bf175f7bd8d12c097aa5
justvitalius <justvitalius@gmail.com>
2013-12-27 05:13:05 +0400

    Improve categories index

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,18 +1,23 @@
 module CategoriesHelper
   def render_subtree node
-    content_tag :div, class: 'col-xs-offset-1' do
-      content_tag(:div, render_node(node), class: '')+
-      node.children.map do |subnode|
-        render_subtree subnode
-      end.join().html_safe
+    content_tag :div, class: 'media' do
+      content_tag(:div, image_tag(node.image.url(:xs)), class: 'pull-left' )+
+      content_tag(:div, class: 'media-body') do
+        render_node(node) +
+        node.children.map do |subnode|
+          render_subtree subnode
+        end.join().html_safe
+      end
     end
   end
 
 
   def render_node node
-    content_tag(:div, node.id, class: 'col-xs-1')+
-    content_tag(:div, node.title, class: 'col-xs-3')+
-    content_tag(:div, node.image_url(:xs), class: 'col-xs-2')+
-    link_to('создать потомка', new_category_path(parent: node))
+    content_tag(:h4, node.title, class: 'media-heading')+
+    content_tag(:div, class: '') do
+      link_to('редактировать', edit_category_path(node), class: 'btn btn-primary')+
+      link_to('удалить', category_path(node), method: :delete, class: 'btn btn-default')+
+      link_to('создать потомка', new_category_path(parent: node), class: 'btn btn-default')
+    end
   end
 end
