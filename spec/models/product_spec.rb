cf63a5e8183be359fff9383ea47caf3778eb2d23
justvitalius <justvitalius@gmail.com>
2014-01-01 20:55:41 +0400

    Add product images tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,22 +2,40 @@ require 'spec_helper'
 
 describe Product do
 
-  it {should respond_to(:title)}
-  it {should validate_presence_of(:title)}
-  it {should ensure_length_of(:title).is_at_most(64)}
+  describe 'it should have title' do
+    it {should respond_to(:title)}
+    it {should validate_presence_of(:title)}
+    it {should ensure_length_of(:title).is_at_most(64)}
+  end
 
 
-  it 'should have image'
+  describe 'should have images' do
+    before do
+      @pr = Product.new(title: 'pr1', description: 'pr1 description', price: 1.00)
+    end
 
-  it {should respond_to(:price)}
-  it {should validate_presence_of(:price)}
-  it {should validate_numericality_of(:price).only_integer}
-  it {should validate_numericality_of(:price).is_less_than_or_equal_to(1000000)}
-  it {should validate_numericality_of(:price).is_greater_than_or_equal_to(1)}
-  it 'should validate price integer parts and cents'
+    it {should respond_to(:images)}
+    it 'should have empty images by default' do
+      expect(@pr.images).to eq([])
+    end
 
-  it {should respond_to(:description)}
-  it {should ensure_length_of(:description).is_at_most(1000)}
+    it 'should have several images' do
+      @pr.images << ImageUploader.new(@pr, :product_image)
+      @pr.images << ImageUploader.new(@pr, :product_image)
+      expect(@pr.images.length).to eq(2)
+    end
+  end
+
+  describe 'it should have price' do
+    it {should respond_to(:price)}
+    it {should validate_presence_of(:price)}
+    it {should validate_presence_of(:price).is_greater_than_or_equal_to BigDecimal.new(0.01)}
+  end
+
+  describe 'it should have description' do
+    it {should respond_to(:description)}
+    it {should ensure_length_of(:description).is_at_most(1000)}
+  end
 
   it 'should have many Auctions'
 end
