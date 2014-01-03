f8db5ce2b9fb4b71254406d51aab449079d803aa
justvitalius <justvitalius@gmail.com>
2014-01-03 07:03:38 +0400

    Add some tests about Category in Product

---
+++ spec/factories/category.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :category do
+    title 'category'
+  end
+end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,9 +4,9 @@ require 'bigdecimal'
 describe Product do
 
   describe 'it should have title' do
-    it {should respond_to(:title)}
-    it {should validate_presence_of(:title)}
-    it {should ensure_length_of(:title).is_at_most(64)}
+    it { should respond_to(:title) }
+    it { should validate_presence_of(:title) }
+    it { should ensure_length_of(:title).is_at_most(64) }
   end
 
 
@@ -15,7 +15,7 @@ describe Product do
       @pr = create(:product)
     end
 
-    it {should have_many(:images).class_name('ProductImage').dependent(:destroy)}
+    it { should have_many(:images).class_name('ProductImage').dependent(:destroy) }
 
     it 'should have empty images by default' do
       expect(@pr.images).to eq([])
@@ -27,16 +27,26 @@ describe Product do
     end
   end
 
-  describe 'it should have price' do
-    it {should respond_to(:price)}
-    it {should validate_presence_of(:price)}
-    it {should validate_numericality_of(:price).is_greater_than_or_equal_to BigDecimal.new('0.01')}
+  describe 'should have price' do
+    it { should respond_to(:price) }
+    it { should validate_presence_of(:price) }
+    it { should validate_numericality_of(:price).is_greater_than_or_equal_to BigDecimal.new('0.01') }
   end
 
-  describe 'it should have description' do
-    it {should respond_to(:description)}
-    it {should ensure_length_of(:description).is_at_most(1000)}
+  describe 'should have description' do
+    it { should respond_to(:description) }
+    it { should ensure_length_of(:description).is_at_most(1000) }
   end
 
-  it {should have_many(:auctions).dependent(:destroy)}
+  describe 'should have category' do
+    it { should belong_to(:category) }
+
+    it 'should clean category when removed category' do
+      @cat = create(:category)
+      @pr = create(:product, category: @cat)
+      @cat.destroy
+      expect(@pr.category).to be_nil
+    end
+  end
+  it { should have_many(:auctions).dependent(:destroy) }
 end
