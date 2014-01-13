b091dab2fadd62a2af208525d35d1201aa5859fe
justvitalius <justvitalius@gmail.com>
2014-01-13 08:09:06 +0400

    Update Auction form.

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,9 @@
 class Admin::BaseController < ApplicationController
   before_filter :authenticate_user!
+
+  helper_method :categories
+
+  def categories
+    @categories ||= Category.all
+  end
 end
