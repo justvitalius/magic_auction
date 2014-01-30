fc50a2bc9ebac1cbe324f135d45bd0e563833ad7
justvitalius <justvitalius@gmail.com>
2014-01-30 11:06:19 +0400

    Refactor strong-parameters in products controller.

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,8 +9,11 @@ class Admin::ProductsController < Admin::ResourcesController
   end
 
   protected
-  def resource_params
-    [params.require(:product).permit(:title, :description, :price, :category_id, images_attributes: ['id', 'image', 'image_cache', 'product_id', '_destroy'])] if params.has_key?(:product)
+  def build_resource_params
+    [params.fetch(:widget, {}).permit(
+         :title, :description, :price, :category_id,
+         images_attributes: ['id', 'image', 'image_cache', 'product_id', '_destroy']
+     )]
   end
 
 end
