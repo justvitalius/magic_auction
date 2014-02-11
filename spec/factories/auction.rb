49cd4c0d79947c47aa3be18fb4fdcd2d06c382ba
justvitalius <justvitalius@gmail.com>
2014-02-12 00:12:29 +0400

    Add non-time-dependence for time-testing with started zeus.

--- spec/factories/auction.rb
+++ spec/factories/auction.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :auction do
     title 'auction'
-    expire_date DateTime.now + 1.month
-    start_date DateTime.now
+    expire_date { DateTime.now + 1.month }
+    start_date { DateTime.now }
     price_step 1.00
     time_step 60
     product
@@ -10,10 +10,17 @@ FactoryGirl.define do
 
     factory :auction_with_images do
       title 'auction'
-      expire_date DateTime.now + 1.month
-      start_date DateTime.now
+      expire_date { DateTime.now + 1.month }
+      start_date { DateTime.now }
 
       product { FactoryGirl.create(:product_with_images) }
+    end
+
+    factory :expires_auction do
+      sequence(:title){ |n| "expired-auction-#{n}" }
+      expire_date { DateTime.now - 1.month }
+      start_date { DateTime.now - 2.months }
+      to_create {|instance| instance.save(validate: false) }
     end
   end
 end
