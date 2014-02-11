e7a3f9d1b19bddc3b7a6863a058de78e50fc67e5
justvitalius <justvitalius@gmail.com>
2014-02-12 00:13:01 +0400

    Add bets specs.

--- spec/models/bet_spec.rb
+++ spec/models/bet_spec.rb
@@ -3,16 +3,39 @@ require 'bigdecimal'
 
 describe Bet do
 
+  let(:bet){ create(:bet) }
+
   describe 'should have relations to user' do
-    pending
+    it { should belong_to(:user).dependent(:destroy) }
+    it { should validate_presence_of(:user_id) }
   end
 
   describe 'should have relations to auction' do
-    pending
+    it { should belong_to(:auction).dependent(:destroy) }
+    it { should validate_presence_of(:auction_id) }
   end
 
-  describe 'should have bet type' do
-    pending
+  describe '#type' do
+    it{ should validate_presence_of(:type) }
+    it{ should validate_numericality_of(:type) }
+
+    it 'should be equal to "1" by default' do
+      expect(Bet.new.type).to eq(1)
+    end
+
+    it 'should be valid only equals "1" or "2"' do
+      [1, 2].each do |type|
+        bet.type = type
+        expect(bet).to be_valid
+      end
+    end
+
+    it 'not should be valid if not equals to 1, 2' do
+      ([0]+[3..rand(100)]).each do |type|
+        bet.type = type
+        expect(bet).to_not be_valid
+      end
+    end
   end
 
 end
