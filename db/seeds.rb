27fbbc0c31c5454cb244b71ba2d61a7423718155
justvitalius <justvitalius@gmail.com>
2014-03-11 22:46:29 +0400

    Add Permission and seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@ User.delete_all
 FactoryGirl.create(:user)
 FactoryGirl.create(:admin)
 10.times { FactoryGirl.create(:auction) }
+
+# Import permissions
+[
+    { title: 'Управление категориями', action: :manage, subject: 'Category'},
+    { title: 'Управление продуктами', action: :manage, subject: 'Product'}
+].each do |permission|
+  title = permission.delete(:title)
+  p = Permission.where(permission).first
+  p.present? ? p.update(title: title) : Permission.create!(permission.merge(title: title))
+end
+
+puts 'permission import done'
+# end of Import permissions
+
+
